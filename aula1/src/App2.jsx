b19b40c385ae1ecfd53fbe3f21a124a50c342b9c
Edney7 <edneiaraujosilva00@gmail.com>
2025-02-11 22:49:38 -0300

    Adicionando a Logo e o título

---
+++ aula1/src/App2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+//rafce
+
+//Arrow function
+//Componentes são nomeados com PascalCase
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado {App2}
